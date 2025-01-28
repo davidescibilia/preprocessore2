86e88137bb9eab514e0d508660c54b71fec80b90
davidescibilia <davide.scibilia@davincimilazzo.edu.it>
2025-01-28 13:08:24 +0100

    Update es n 2 preprocessore.c

--- es tpsi preprocessore/es n 2 preprocessore.c
+++ es tpsi preprocessore/es n 2 preprocessore.c
@@ -3,7 +3,9 @@
 #define ABSOLUTE(num) abs(num)
 int main(){
 	printf("%d",ABSOLUTE(-1));
-	
+	printf("%d",ABSOLUTE(3 - 4));
+	printf("%d",ABSOLUTE(7));
+	printf("%d",ABSOLUTE(-8 - 3));
 return 0;
 }
 
